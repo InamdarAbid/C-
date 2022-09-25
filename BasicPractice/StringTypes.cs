f7028b9e0fe5e48ae69e488ab6315c0bfa8a7419
InamdarAbid <inamdarabid7@gmail.com>
2022-09-25 09:20:00 +0530

    Basic String

---
+++ BasicPractice/StringTypes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPractice
+{ 
+    /// <summary>
+    /// This Class has code for basic String types in C#
+    /// </summary>
+    public class StringTypes
+    {
+        public static void PrintStrings()
+        {
+            string Name = "Abid";
+            Name = "\"Abid"; //Use / as escape character  
+            Console.WriteLine(Name);
+            Name = @"C:\Windows\System32\cmd.exe"; //If you use @ at the  start you don't have to use escape chatacters at the begining
+            Console.WriteLine(Name);
+            Console.WriteLine(" 1 \n 2 \n 3");
+        }
+
+    }
+}
