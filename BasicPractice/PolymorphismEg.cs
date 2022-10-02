0d942e71d84040406e2a1e7f1ef750345634a14f
InamdarAbid <inamdarabid7@gmail.com>
2022-10-02 12:08:46 +0530

    Polymorphism

---
+++ BasicPractice/PolymorphismEg.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPractice
+{
+    public class PolymorphismEg
+    {
+        Example of runtime Polymorphism
+        Employee[] e = new Employee[4];
+        e[0] = new Employee();
+        e[1] = new PartTimeEmployee();
+        e[2] = new FullTimeEmployee();
+        e[3] = new TemperoryTimeEmployee();
+
+        foreach(Employee empl in e)
+        {
+            empl.PrintFullName();
+        }
+}
+
+    public static class CompiletimePolymorphism
+    {
+        public static void area(int side)
+        {
+            Console.WriteLine("Area of squeare : {0}",(side * side));
+        }
+
+        public static void area(int l,int b)
+        {
+            Console.WriteLine("Area of rectangle : {0}", (l * b));
+        }
+    }
+
+}
