c433494ef58a57004e40d483d8ed664f00063cde
InamdarAbid <inamdarabid7@gmail.com>
2022-10-03 07:15:19 +0530

    Structure

---
+++ BasicPractice/Structure.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicPractice
+{
+    internal class Structure
+    {
+        public struct Stud
+        {
+            private int _id;
+            private string _name;
+
+            public int Id
+            {
+                get { return this._id; }
+                set { this._id = value; }
+            }
+            public string Name
+            {
+                get { return this._name; }
+                set { this._name = value; }
+            }
+
+            public Stud(int id, string name)
+            {
+                this._id = id;
+                this._name = name;
+            }
+
+            public void PrintStudent()
+            {
+                Console.WriteLine("{0} and {1}", this._id, this._name);
+            }
+        }
+    }
+}
